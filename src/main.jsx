3d9d479a68bf472fedec5f5cbb18933df7873eb8
Sandrianna <sashagoncharik6@gmail.com>
2025-03-14 13:59:05 +0300

    chore: delete provider and add store

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./styles/main.css";
-import { ErrorProvider } from "./context/ErrorProvider.jsx";
+import { Provider } from "react-redux";
 import { AuthProvider } from "./context/AuthProvider.jsx";
 import App from "./App.jsx";
+import store from "./store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
-      <ErrorProvider>
+      <Provider store={store}>
         <App />
-      </ErrorProvider>
+      </Provider>
     </AuthProvider>
   </BrowserRouter>
 );
