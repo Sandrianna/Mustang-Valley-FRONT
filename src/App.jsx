948099d0bc1346da9657abf09cfc89d325cfac64
Sandrianna <sashagoncharik6@gmail.com>
2025-03-14 13:57:50 +0300

    chore: delete providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { Routes, Route } from "react-router";
-import useLogIn from "./hooks/useLogIn.jsx";
+import useLogIn from "./hooks/useLogIn.js";
 import HomePage from "./pages/HomePage.jsx";
-import { SnackbarProvider } from "./context/SnackbarProvider.jsx";
-import { LoadingProvider } from "./context/LoadingProvider.jsx";
 import Home from "./components/Home.jsx";
 import Gallery from "./pages/Gallery.jsx";
 import Profile from "./pages/Profile.jsx";
@@ -17,38 +15,10 @@ export default function App() {
       <Home />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route
-          path="/gallery"
-          element={
-            <LoadingProvider>
-              <Gallery />
-            </LoadingProvider>
-          }
-        />
-        <Route
-          path="/login"
-          element={
-            <SnackbarProvider>
-              <Login />
-            </SnackbarProvider>
-          }
-        />
-        <Route
-          path="/registration"
-          element={
-            <SnackbarProvider>
-              <SignUp />
-            </SnackbarProvider>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <LoadingProvider>
-              <Profile />
-            </LoadingProvider>
-          }
-        />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   );
