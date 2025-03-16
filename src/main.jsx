3cf8923f65aa7bbcee90c35eb1cd5f3a29bc0de0
Sandrianna <sashagoncharik6@gmail.com>
2025-03-16 20:36:14 +0300

    chore: delete AuthProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./styles/main.css";
 import { Provider } from "react-redux";
-import { AuthProvider } from "./context/AuthProvider.jsx";
 import App from "./App.jsx";
 import store from "./store/store.ts";
+import SnackbarComponent from "./components/SnackbarComponent.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AuthProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </AuthProvider>
+    <Provider store={store}>
+      <App />
+      <SnackbarComponent />
+    </Provider>
   </BrowserRouter>
 );
