2b19423b91cf90162231d4660ec75ed7a3b39597
Sandrianna <sashagoncharik6@gmail.com>
2025-03-17 20:24:38 +0300

    fix: small fixes

--- src/hooks/useLogIn.js
+++ src/hooks/useLogIn.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router";
 import { useDispatch } from "react-redux"; 
 import { showSnackbar } from "../store/snackbarSlice.ts";
 
-export default function useLogIn() {
+export function useLogIn() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -18,6 +18,9 @@ export default function useLogIn() {
           dispatch(showSnackbar("Неверный логин или пароль!"));
  
           navigate("/login");
+        }
+        else if(error.response.status === 400) {
+          dispatch(showSnackbar("Уже существует пользователь с таким логином!"));
         }
         return Promise.reject(error);
       }
