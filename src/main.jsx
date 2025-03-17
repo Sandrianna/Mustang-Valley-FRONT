d48f331e0a4b318f05796a88c5f201ea30a5dd4d
Sandrianna <sashagoncharik6@gmail.com>
2025-03-17 20:25:12 +0300

    fix: small fixes

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./styles/main.css";
 import { Provider } from "react-redux";
-import App from "./App.jsx";
-import store from "./store/store.ts";
-import SnackbarComponent from "./components/SnackbarComponent.jsx";
+import { App } from "./App.jsx";
+import store from "./store/store.js";
+import { SnackbarComponent } from "./components/SnackbarComponent.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
