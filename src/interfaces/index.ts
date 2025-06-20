f4779d8f8fd5d58caeb08cc1aabc29cf6ade7486
Sandrianna <sashagoncharik6@gmail.com>
2025-06-20 12:34:46 +0300

    feat: add interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -3,6 +3,13 @@ import {store} from "../store/store.ts"
 
 export type AppDispatch = typeof store.dispatch;
 
+export interface RootState {
+  admin: AdminState;
+  booking: BookingState;
+  profile: ProfileState;
+  login: LoginState;
+}
+
 export interface GalleryValues {
     images: string[];
     status: 'idle' | 'loading' | 'succeeded' | 'failed';
@@ -13,7 +20,7 @@ export interface GalleryState {
 }
 
 export interface LoginValues {
-    user: null | string | void;
+    user: {username: string, role: string} |null ;
     status: 'idle' | 'loading' | 'succeeded' | 'failed';
 }
 
@@ -44,10 +51,126 @@ export interface SnackbarState {
     snackbar: SnackbarValues;
 }
 
+export interface Booking {
+  _id: string;
+  userId: string;
+  checkInDate: Date;
+  checkOutDate: Date;
+  option: string;
+  adults: number;
+  children: number;
+  hasPets: boolean;
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+export interface BookingWithUserDetails {
+  _id: string;
+  userId: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+  checkInDate: Date;
+  checkOutDate: Date;
+  option: string;
+  adults: number;
+  children: number;
+  hasPets: boolean;
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export type BookingResponse = Booking | BookingWithUserDetails;
+
+export interface BookingState {
+  booking: BookingResponse | null;
+  bookings: BookingResponse[];
+  status: 'idle' | 'loading' | 'succeeded' | 'failed';
+  error: string | null;
+  availability: {
+    status: 'idle' | 'loading' | 'succeeded' | 'failed';
+    isAvailable: boolean;
+    error: string | null;
+    unavailableDates: Date[];
+  };
+}
+
+export interface BookingData {
+  checkInDate: Date | null;
+  checkOutDate: Date | null;
+  option: string;
+  adults: number;
+  children: number;
+  hasPets: boolean;
+}
+
+
+export interface ExtendedUserForm {
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  phone: string;
+  gender: "male" | "female";
+  password: string;
+}
+
 export interface Username {
+    _id: string;
     username: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    gender: "male" | "female";
+    avatar?: string;
+    favoriteOptions?: string;
+    specialWishes?: string;
+}
+
+
+
+export interface ExtendedUserForm {
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  phone: string;
+  gender: "male" | "female";
+  password: string;
 }
 
 export interface UsernamePassword extends Username{
     password: string;
+    phone: string;
+
+}
+
+export interface SmsValues {
+    phone: string;
+    code: string;
+    sessionId: string | null;
+    status: string,
+}
+
+export interface SmsState {
+    sms: SmsValues;
+}
+
+export interface User {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  role: string;
+}
+
+
+export interface AdminState {
+  users: User[];
+  status: 'idle' | 'loading' | 'succeeded' | 'failed';
+  error: string | null;
 }
